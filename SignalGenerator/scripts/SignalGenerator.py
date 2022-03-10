524953611ae16e6d7ffa5602a607560cab6ac575
Bernhard Linux <bgirsule@gmail.com>
2022-03-10 17:24:56 +0100

    SignalGenerator V3 - Two Model Prediction

--- SignalGenerator/scripts/SignalGenerator.py
+++ SignalGenerator/scripts/SignalGenerator.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Version 3.0
+# - Two-model prediciton, including daily data
+# - Strategy 2022_RG_2
 # # Version 2.0
 # - Using the remote predictor (with model CreateModelStraightLSTMPyramidBiggerFloat_R64_E64_P64_256LB_24GLAHD_SM200_20/cp_8_06000)
 # - Strategy 2022_D1
@@ -19,17 +22,23 @@
 
 # In[ ]:
 
+import sys
+sys.path.insert(0, "../../IndicatorCalculator")
+sys.path.insert(0, "../../DataStreamCreator")
 
-import requests
-import numpy as np
-import pandas as pd
-import json
-from datetime import datetime
-import time
-import talib
-import os
-import json
+# Import custom modules
 from dotenv import load_dotenv
+import os
+import talib
+import time
+from datetime import datetime
+import json
+import pandas as pd
+import numpy as np
+import requests
+import DataStreamCreator as dg
+from IndicatorCalculator import IndicatorCalculator, IndicatorCalculationError
+import logging
 
 
 # In[ ]:
@@ -44,22 +53,8 @@ from dotenv import load_dotenv
 # In[ ]:
 
 
-import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
-# In[ ]:
-
-
-# Import custom modules
-import sys
-
-sys.path.insert(0, "../../IndicatorCalculator")
-sys.path.insert(0, "../../DataStreamCreator")
-
-from IndicatorCalculator import IndicatorCalculator, IndicatorCalculationError
-import DataStreamCreator as dg
 
 
 # In[ ]:
@@ -71,7 +66,8 @@ load_dotenv("../../.env")
 
 MARKETS_URL = "https://api.bittrex.com/v3/markets"
 MARKETS_SUMMARIES_URL = "https://api.bittrex.com/v3/markets/summaries"
-TICK_URL = "https://api.bittrex.com/v3/markets/{}/candles/TRADE/HOUR_1/recent"
+TICK_URL_HOURLY = "https://api.bittrex.com/v3/markets/{}/candles/TRADE/HOUR_1/recent"
+TICK_URL_DAILY = "https://api.bittrex.com/v3/markets/{}/candles/TRADE/DAY_1/recent"
 
 
 # In[ ]:
@@ -91,32 +87,35 @@ if PREDICTOR_REST_SERVICE_URL is None:
 # In[ ]:
 
 SIGNAL_FOLDER = 'signals'
-MIN_CURR_PROCESS_TIME_SECONDS = 2
-VOL_LIMIT = 200000.0
+MIN_CURR_PROCESS_TIME_SECONDS = 3
+VOL_LIMIT = 100000.0
 
 
 # In[ ]:
 
-# Todo: Check
-ENTR_THR = -0.2
-ENTR_THR2 = 0.04
-EXIT_THR = 0.9
+ENTR_THR = 0.6
+ENTR_THR2 = 0.1
+EXIT_THR = -0.5
 
 
 # In[ ]:
 
+SHORTSPAN = 6
+MIDSPAN = 48
+LONGSPAN = 120
 
 #BATCH_SIZE = 96
-INDICATOR_MA_PERIOD = 24
-LOOKBACK_CNT = 256
-FEATURES = 228
-PATTERN_CNT = 61
+# Only required if using span p values, else set to 2 for current and previous value
+INDICATOR_MA_PERIOD = 2
 
+MODEL1_X_LOOKBACK_CNT = 512
+MODEL2_X_LOOKBACK_CNT = 256
 
 # In[ ]:
 
 
-IGNORE_STRINGS = os.getenv('IGNORE_STRINGS') #["BULL", "BEAR", "TSLA", "ZUSD", "USDN", "BTC", "ETH", "XELS"]
+# ["BULL", "BEAR", "TSLA", "ZUSD", "USDN", "BTC", "ETH", "XELS"]
+IGNORE_STRINGS = os.getenv('IGNORE_STRINGS')
 if IGNORE_STRINGS is None:
     raise Exception("IGNORE_STRINGS is None")
 else:
@@ -132,12 +131,12 @@ def GetMarketNames():
     r = requests.get(MARKETS_URL)
     j = r.json()
     markets = pd.DataFrame(j)
-    
+
     # Filter the markets
     markets = markets[markets['status'] == "ONLINE"]
     markets = markets[markets['quoteCurrencySymbol'] == "USDT"]
     marketsList = markets['symbol'].values
-        
+
     return marketsList
 
 
@@ -149,16 +148,18 @@ def GetMarketSummaries():
     r = requests.get(MARKETS_SUMMARIES_URL)
     j = r.json()
     marketsSummaries = pd.DataFrame(j)
-    marketsSummaries.set_index("symbol",inplace=True)
-    
-    marketsSummaries["quoteVolume"] = pd.to_numeric(marketsSummaries["quoteVolume"], downcast="float")
-    marketsSummaries["percentChange"] = pd.to_numeric(marketsSummaries["percentChange"], downcast="float")
-    
+    marketsSummaries.set_index("symbol", inplace=True)
+
+    marketsSummaries["quoteVolume"] = pd.to_numeric(
+        marketsSummaries["quoteVolume"], downcast="float")
+    marketsSummaries["percentChange"] = pd.to_numeric(
+        marketsSummaries["percentChange"], downcast="float")
+
     # Filter the markets
 #     markets = markets[markets['status'] == "ONLINE"]
 #     markets = markets[markets['quoteCurrencySymbol'] == "USDT"]
 #     marketsList = markets['symbol'].values
-        
+
     return marketsSummaries
 
 
@@ -167,37 +168,38 @@ def GetMarketSummaries():
 # In[ ]:
 
 
-def GetTicks(market):
-    u = TICK_URL.format(market)
+def GetTicks(market, tickUrl):
+    u = tickUrl.format(market)
     r = requests.get(u)
     r.json()
     j = r.json()
-    
+
     if 'code' in j:
         logging.error("Error in API response")
         logging.error(j)
         logging.error(u)
-        
+
         return None
-        
+
     # Refurbish values
     for i in range(len(j)):
         try:
-            j[i]['startsAt'] = int(datetime.strptime(j[i]['startsAt'], '%Y-%m-%dT%H:%M:%SZ').timestamp())
+            j[i]['startsAt'] = int(datetime.strptime(
+                j[i]['startsAt'], '%Y-%m-%dT%H:%M:%SZ').timestamp())
             j[i]['open'] = float(j[i]['open'])
             j[i]['high'] = float(j[i]['high'])
             j[i]['low'] = float(j[i]['low'])
             j[i]['close'] = float(j[i]['close'])
             j[i]['volume'] = float(j[i]['volume'])
             j[i]['quoteVolume'] = float(j[i]['quoteVolume'])
-        except KeyboardInterrupt: 
+        except KeyboardInterrupt:
             raise
         except Exception as ex:
             logging.error(u)
             logging.error(j[i])
             logging.error(ex)
             return None
-            
+
     return j
 
 
@@ -205,33 +207,41 @@ def GetTicks(market):
 
 
 # Init IndicatorCalculator
-ic = IndicatorCalculator()
+ic = IndicatorCalculator(
+    shortspan=SHORTSPAN, midspan=MIDSPAN, longspan=LONGSPAN)
 
 # # Signals
 
 # In[ ]:
 
 
-def GenerateSignals(p, quoteVolume):    
-    p_dir_span = talib.MA(p[:,0].astype(float), timeperiod=INDICATOR_MA_PERIOD)
-    p_dir2nd_span = talib.MA(p[:,1].astype(float), timeperiod=INDICATOR_MA_PERIOD)
+def GenerateSignals(p1, p2, pday, quoteVolume):
+    # BASE_PATH2 == short signals
 
-    p_dir_span_previous = p_dir_span[-2]
-    p_dir2nd_span_previous = p_dir2nd_span[-2]
-    
-    p_dir_span = p_dir_span[-1]
-    p_dir2nd_span = p_dir2nd_span[-1]
+    p1_dir = p1[-1, 0]
+    p1_dir_previous = p1[-2, 0]
 
-    # 2022_D1
-    _entr = (p_dir_span_previous > ENTR_THR) & (p_dir_span <= ENTR_THR) & (p_dir2nd_span <= ENTR_THR2) & (quoteVolume >= VOL_LIMIT)
-    _exit = (p_dir_span_previous < EXIT_THR) & (p_dir_span >= EXIT_THR)
+    p2_dir = p2[-1, 0]
+    p2_dir_previous = p2[-2, 0]
+
+    pday_dir = pday[-1, 0]
+
+    # _2022_RG_2
+    _entr = ((p2_dir - p1_dir) >= ENTR_THR) & ((p2_dir_previous - p1_dir_previous)
+                                               < ENTR_THR) & (pday_dir >= ENTR_THR2) & (quoteVolume >= VOL_LIMIT)
+    _exit = ((p2_dir - p1_dir) <=
+             EXIT_THR) & ((p2_dir_previous - p1_dir_previous) > EXIT_THR)
 
     if _entr or _exit:
-        print(p_dir_span)
-        print(p_dir_span_previous)
-
-        print(p_dir2nd_span)
-        print(p_dir2nd_span_previous)
+        logging.info({
+            'entry': _entr,
+            'exit': _exit,
+            'p1_dir': p1_dir,
+            'p1_dir_previous': p1_dir_previous,
+            'p2_dir': p2_dir,
+            'p2_dir_previous': p2_dir_previous,
+            'pday_dir': pday_dir
+        })
 
     return {
         "entry": _entr,
@@ -246,19 +256,21 @@ def WriteSignalFile(cur, direction):
     if not os.path.exists(SIGNAL_FOLDER):
         logging.error("Signal folder %s does not exist!" % SIGNAL_FOLDER)
         return
-    
+
     try:
-        cur_clean = cur.replace("-USDT","")
-        fname = str(int(datetime.utcnow().timestamp())) + "_" + str(direction) + "_" + cur_clean
+        cur_clean = cur.replace("-USDT", "")
+        fname = str(int(datetime.utcnow().timestamp())) + \
+            "_" + str(direction) + "_" + cur_clean
         f = open(SIGNAL_FOLDER + os.path.sep + fname, "w")
         f.close()
-    except KeyboardInterrupt: 
+    except KeyboardInterrupt:
         raise
     except Exception as ex:
         logging.error("Exception at writing signal file for " + str(cur))
         logging.error(ex)
-        
-WriteSignalFile('i-am-a-debug-signal','entry-or-exit')
+
+
+WriteSignalFile('i-am-a-debug-signal', 'entry-or-exit')
 
 
 # In[ ]:
@@ -266,7 +278,8 @@ WriteSignalFile('i-am-a-debug-signal','entry-or-exit')
 
 def SendTelegramInfo(message):
     try:
-        message = TELEGRAM_INFO_URL + datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S") + '\n' + message
+        message = TELEGRAM_INFO_URL + datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S") + \
+            '\n' + message
         requests.post(message)
     except Exception as ex:
         logging.error("Exception in sending telegram message")
@@ -291,10 +304,10 @@ for m in marketsList:
         if ign in m:
             _ignore = True
             break
-    
+
     if not _ignore:
         suitable_markets.append(m)
-        
+
 logging.info("len(suitable_markets): " + str(len(suitable_markets)))
 
 
@@ -307,62 +320,117 @@ logging.info("len(ms): " + str(len(ms)))
 
 # In[ ]:
 
+def PrepareTickData(rawTickData):
+    _ticksDF = pd.DataFrame(rawTickData)
+    _ticksDF.set_index("startsAt", inplace=True)
+    _ticksDF.sort_index(inplace=True)
+
+    # Drop latest (instable) index
+    _ticksDF = _ticksDF.iloc[:-1, :]
+
+    # Drop Quote Volume
+    _ticksDF = _ticksDF.drop("quoteVolume", axis=1)
+
+    _indDF = ic.CreateAllIndicatorsTable(_ticksDF)
+    _normedDF = ic.NormPriceRelated(_indDF)
+
+    return _normedDF
+
+# In[]:
+
+
+def GetRemotePrediction(normedDFIn, modelNr):
+    if 1 == modelNr:
+        _X_LBCNT = MODEL1_X_LOOKBACK_CNT
+    elif 2 == modelNr:
+        _X_LBCNT = MODEL2_X_LOOKBACK_CNT
+    else:
+        return None
+
+    # Plus the MA period and one before, to calc previous of MA
+    normedDFSlice = normedDFIn.tail(
+        _X_LBCNT + INDICATOR_MA_PERIOD + 1)
+
+    # Block Generator
+    try:
+        xg = dg.XBlockGenerator(
+            normedDFSlice, normedDFSlice.shape[0], _X_LBCNT)
+        X = next(xg)
+    except StopIteration:
+        logging.warning("StopIteration occured for " + m)
+        return "continue"
+
+    # Create JSON
+    j = {"modelNumber": modelNr,
+         "data": X.tolist()
+         }
+
+    try:
+        r = requests.post(PREDICTOR_REST_SERVICE_URL + "/predict", json=j)
+    except requests.exceptions.ConnectionError as cex:
+        logging.error("Connection Error to predictor", cex)
+        # Todo: Send error message via telegram
+        return None
+
+    if 200 != r.status_code:
+        logging.error("200 != r.status_code, it is " + str(r.status_code))
+
+    content = r.content
+    j_content = json.loads(content)
+
+    if "error" in j_content:
+        logging.error(j_content["error"])
+        return None
+
+    # Extract predictions
+    p = np.array(j_content)
+
+    return p
+
+# In[ ]:
+
+
 logging.info(datetime.now())
 
 for m in suitable_markets:
     startTime = datetime.utcnow().timestamp()
     try:
         logging.info(m)
-        ticks = GetTicks(m)
-        tickDF = pd.DataFrame(ticks)
-        tickDF.set_index("startsAt", inplace = True)
-        tickDF.sort_index(inplace = True)
+        ticksHourly = GetTicks(m, TICK_URL_HOURLY)
+        normedDFHourly = PrepareTickData(ticksHourly)
 
-        # Drop latest (instable) index
-        tickDF = tickDF.iloc[:-1, :]
+        # Predict daily
+        ticksDaily = GetTicks(m, TICK_URL_DAILY)
+        normedDFDaily = PrepareTickData(ticksDaily)
 
-        # Drop Quote Volume
-        tickDFNoQuote = tickDF.drop("quoteVolume", axis=1)
-
-        indDF = ic.CreateAllIndicatorsTable(tickDFNoQuote)
-        normedDF = ic.NormPriceRelated(indDF)
-        normedDFSlice = normedDF.tail(LOOKBACK_CNT + INDICATOR_MA_PERIOD + 1) # Plus the MA period and one before, to calc previous of MA
-
-        # Block Generator
-        try:
-            xg = dg.XBlockGenerator(normedDFSlice, normedDFSlice.shape[0])        
-            X = next(xg)
-        except StopIteration:
-            logging.warning("StopIteration occured for " + m)
+        # Model 2 daily prediction
+        pday = GetRemotePrediction(normedDFDaily, 2)
+        if pday is None:
+            break
+        elif isinstance(pday, str) and "continue" == pday:
             continue
 
-        try:
-            r = requests.post(PREDICTOR_REST_SERVICE_URL + "/predict", json = X.tolist())            
-        except requests.exceptions.ConnectionError as cex:
-            logging.error("Connection Error to predictor", cex)
-            # Todo: Send error message via telegram
+        # Model 1 prediction
+        p1 = GetRemotePrediction(normedDFHourly, 1)
+        if p1 is None:
             break
-        
-        if 200 != r.status_code:
-            logging.error("200 != r.status_code, it is " + str(r.status_code))
-        
-        content = r.content
-        j_content = json.loads(content)
+        elif isinstance(p1, str) and "continue" == p1:
+            continue
 
-        if "error" in j_content:
-            logging.error(j_content["error"])
+        # Model 2 prediction
+        p2 = GetRemotePrediction(normedDFHourly, 2)
+        if p2 is None:
             break
-
-        # Extract predictions        
-        p = np.array(j_content)
+        elif isinstance(p2, str) and "continue" == p2:
+            continue
 
         # Get 24h data
         qv = ms.loc[m, "quoteVolume"]
         # pc = ms.loc[m, "percentChange"] / 100.0
-        
+
         # Generate Signals
-        signals = GenerateSignals(p, qv)       
-        
+        signals = GenerateSignals(p1, p2, pday, qv)
+
         if signals['exit']:
             msg = m + ' exit signal'
             logging.info(msg)
@@ -379,15 +447,15 @@ for m in suitable_markets:
 
             SendTelegramInfo(msg)
             WriteSignalFile(m, 'entry')
-        
-    except KeyboardInterrupt: 
+
+    except KeyboardInterrupt:
         raise
     except Exception as ex:
         logging.error("General Exception in " + str(m))
         logging.error(ex)
-            
+
     elapsed = datetime.utcnow().timestamp() - startTime
-    waitTime = np.max([0.0, MIN_CURR_PROCESS_TIME_SECONDS - elapsed])  
+    waitTime = np.max([0.0, MIN_CURR_PROCESS_TIME_SECONDS - elapsed])
     time.sleep(waitTime)
 
 logging.info(datetime.now())
